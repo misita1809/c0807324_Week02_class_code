e7157294045619251b5eee685ab6fd8bf373aca8
misita1809 <mishitapatel1809@gmail.com>
2021-05-26 16:51:08 +0530

    Initial commit. Module name area_of_circle contains function area

---
+++ area_of_circle.py
@@ -0,0 +1,3 @@
+# This function accept the radius of a circle from the user and compute the area(week2)
+def area(rad):
+    return 3.14*rad*rad
---
+++ main.py
@@ -0,0 +1,9 @@
+"""Name: Misita Sankhala
+Std Id: C0807324
+Emerging Technologies
+Week02"""
+
+import area_of_circle
+
+radius = float(input("Enter Radius of Circle: "))
+print(f"Area of Circle with Radius {radius} is {area_of_circle.area(radius)}")
